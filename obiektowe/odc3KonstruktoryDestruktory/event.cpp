7bf967f1394ea305063e753cbd10836bd6074bf0
kazdyrkeicam <45139068+kazdyrkeicam@users.noreply.github.com>
2019-02-25 09:17:36 +0100

    Update event.cpp

--- obiektowe/odc3KonstruktoryDestruktory/event.cpp
+++ obiektowe/odc3KonstruktoryDestruktory/event.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include "event.h"
 
+using namespace std;
+
+Event::Event(string n, int d, int m, int y, int h, int mins){
+    name = n;
+    day = d;
+    month = m;
+    year = y;
+    hour = h;
+    minute = mins;
+}
+
+Event::~Event(){
+    cout << endl << "to ja destruktor sie nazywam";
+}
+
+void Event::show(){
+    cout << name << " " << day << "." << month << "." << year << " " << hour << ":" << minute << endl;
+}
