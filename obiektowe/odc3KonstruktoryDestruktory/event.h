06e6963defd6641f1df05e3556f98802dc1d2a5c
kazdyrkeicam <45139068+kazdyrkeicam@users.noreply.github.com>
2019-02-25 09:17:10 +0100

    Update event.h

--- obiektowe/odc3KonstruktoryDestruktory/event.h
+++ obiektowe/odc3KonstruktoryDestruktory/event.h
@@ -1,1 +1,16 @@
+#include <iostream>
 
+using namespace std;
+
+class Event{
+  int day, month, year;
+  int hour, minute;
+  string name;
+  
+    public:
+  //nizej wpisane wartosci domyslne
+  Event(string = "brak", int = 1, int = 1, int = 2015, int = 0, int = 0); //gdy przy wywolaniu w main nie poda sie wartosci
+  ~Event();
+  void load();
+  void show();
+};
