a3c743c9f192e7d71f91550b92e34dc46366f411
kazdyrkeicam <45139068+kazdyrkeicam@users.noreply.github.com>
2019-03-06 21:36:06 +0100

    Create main.cpp

---
+++ obiektowe/odc4Friendship/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "friend.h"
+
+using namespace std;
+
+int main(){
+    Punkt pkt1("A", 3, 1);
+    
+    Prostokat p1("Prostokat", 0, 0, 6, 4);
+    
+    return 0;
+}
