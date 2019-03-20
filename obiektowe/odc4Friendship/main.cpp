636d3771692946d311941f501ba99fdb4082de7a
kazdyrkeicam <45139068+kazdyrkeicam@users.noreply.github.com>
2019-03-20 23:53:33 +0100

    Update main.cpp

--- obiektowe/odc4Friendship/main.cpp
+++ obiektowe/odc4Friendship/main.cpp
@@ -3,10 +3,21 @@
 
 using namespace std;
 
+void sedzia(Punkt s, Prostokat p){
+
+    if((s.x >= p.x) && (s.x <= p.x + p.szerokosc) &&
+       (s.y >= p.y) && (s.y <= p.y + p.wysokosc))
+
+        cout << endl << "punkt " << s.nazwa << " nalezy do " << p.nazwa;
+    else
+        cout << endl << "punkt " << s.nazwa << " nie nalezy do " << p.nazwa;
+ }
+
 int main(){
     Punkt pkt1("A", 3, 1);
     
     Prostokat p1("Prostokat", 0, 0, 6, 4);
     
+    sedzia(pkt, p);
     return 0;
 }
